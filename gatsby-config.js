acbe8fd73d75523c6541cf64c348aeb9b24caef1
young <somzero.design@gmail.com>
2019-07-11 16:38:43 +0900

    개츠비 셋팅

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,21 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Zelog blog`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,96 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../component/layout"
+// import BlogTitle from "../components/blogTitle"
+import Img from "gatsby-image"
+import styled from "styled-components"
 
-export default () => <div>Hello world!</div>
+const Posts = styled.div`
+  padding: 0 14vw;
+  color: #b9b9b9;
+`
+
+const PostBox = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+  color: inherit;
+`
+
+const PostTitle = styled.div`
+  font-size: 18px;
+`
+
+const PostBoxDescription = styled.div`
+  margin-top: 8px;
+  font-size: 14px;
+`
+
+const PostImageBox = styled.div`
+  margin-top: 32px;
+  height: 190px;
+`
+
+const PostImage = styled(Img)`
+  max-width: 100%;
+  max-height: 100%;
+  margin: auto;
+  display: block;
+`
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      {/* <BlogTitle /> */}
+      <Posts>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <PostBox key={node.id} to={node.fields.slug}>
+            <PostTitle>{node.frontmatter.title}</PostTitle>
+            <PostBoxDescription>
+              {node.frontmatter.description}
+            </PostBoxDescription>
+            <PostImageBox
+              style={{
+                width: `190px`,
+              }}
+            >
+              <PostImage
+                fluid={node.frontmatter.featuredImage.childImageSharp.fluid}
+              />
+            </PostImageBox>
+          </PostBox>
+        ))}
+      </Posts>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___publishedAt], order: DESC }
+    ) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            description
+            featuredImage {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
