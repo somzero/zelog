be64210844f37ca39f42191e6716c806d56b09e5
young <48948744+somzero@users.noreply.github.com>
2019-08-19 00:35:21 +0900

    setting

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,12 +1,18 @@
 import React from "react"
-import './header.css'
+import "./header.css"
 
 const Header = () => (
   <div className="header">
-    <div className="header__item" style={{color: `#ffae00`}}>Blog</div>
-    <div className="header__item" style={{color: `#b9b9b9`}}>Project</div>
-    <div className="header__item" style={{color: `#b9b9b9`}}>About</div>
+    <div className="header__item" style={{ color: `#ffae00` }}>
+      Blog
+    </div>
+    <div className="header__item" style={{ color: `#b9b9b9` }}>
+      Project
+    </div>
+    <div className="header__item" style={{ color: `#b9b9b9` }}>
+      About
+    </div>
   </div>
 )
 
-export default Header;
+export default Header
--- src/component/layout/layout.js
+++ src/component/layout/layout.js
@@ -1,37 +1,43 @@
+import Header from "../header/header"
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { StaticQuery, graphql } from "gatsby"
-import Header from "../header/header"
-import './layout.css'
+import "./layout.css"
 
 const Layout = ({ children }) => (
-    <StaticQuery
-        query={graphql`
-            query SiteTitleQuery {
-                site {
-                    siteMetadata {
-                        title
-                    }
-                }
-            }
-        `}
-        render={data => (
-            <>
-                <Helmet 
-                    title={data.site.siteMetadata.title} 
-                    meta={[{ name: 'viewport', content: 'width=device-width, user-scalable=1' }]}>
-                    <html lang="ko" />
-                </Helmet>
-                    <Header />
-                    <div className="contents">{children}</div>
-            </>
-        )}
-    />
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            {
+              name: "viewport",
+              content: "width=device-width, user-scalable=1",
+            },
+          ]}
+        >
+          <html lang="ko" />
+        </Helmet>
+        <Header />
+        <div className="contents">{children}</div>
+      </>
+    )}
+  />
 )
 
 Layout.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 }
 
 export default Layout
